753af085ccdc6d4ee9824cd7ff7d187b67f301f9
Xsasidu <sadarusadaru292@gmail.com>
2024-05-21 17:40:53 +0530

    pragith md

---
+++ config-env.js
@@ -0,0 +1,34 @@
+/**
+* @project_name pragith md 
+* @authors Prabath Kumara & sachibot-team(Sachintha) <https://github.com/Xsasidu>
+* @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
+* @link <https://github.com/Xsasidu/pragith md>
+* @version 2.0.6
+
+© 2023 , pragith md. All rights reserved.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.*/
+
+* @project_name PRABATH-MD
+* @authors Prabath Kumara & sachibot-team(Sachintha) <https://github.com/prabathLK>
+* @description The main hope of creating this bot is to take full advantage of the WhatsApp app and make its work easier
+* @link <https://github.com/prabathLK/PRABATH-MD>
+* @version 2.0.6
+
+© 2023 Prabath Kumara, SACHI-BOT. All rights reserved.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.*/
+
+const fs = require('fs');
+if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
+
+function convertToBool(text, fault = 'true') {
+    return text === fault ? true : false;
+}
+module.exports = {
+SESSION_ID: process.env.SESSION_ID || "Enter your session-id",
+BOT_NUMBER: process.env.BOT_NUMBER || "Enter your botnumber (without + mark and spaces)",
+PASSWORD: process.env.PASSWORD || "Enter you like password",
+GITHUB_USERNAME: process.env.GITHUB_USERNAME || "Enter your github username",
+GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN || "Enter your GITHUB_AUTH_TOKEN"
+};
